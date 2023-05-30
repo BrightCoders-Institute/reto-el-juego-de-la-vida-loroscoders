718eec51e802a8db02c558c405434596da20c2b0
DerekAyala <derek.ayala12345@hotmail.com>
2023-05-29 19:08:56 -0600

    Added functions for random grid generation and board display
    Created the "createGrid" function to generate a grid with randomly assigned living and dead cells.
    Implemented the "printGrid" function to visually display the game board.
    Co-authored-by: Tole15 <rodrigermoon@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-class GameOfLine{
-  constructor(rows, columns){
+class GameOfLife {
+  constructor(rows, columns) {
     this.rows = rows;
     this.columns = columns;
+    this.grid = this.createGrid();
+  }
+
+  createGrid() {
+    const grid = new Array(this.row);
+    for (let i = 0; i < this.rows; i++) {
+      grid[i] = new Array(this.columns);
+      for (let j = 0; j < this.columns; j++) {
+        grid[i][j] = Math.random() < 0.5 ? "." : "*";
+      }
+    }
+    return grid;
+  }
+
+  printGrid() {
+    for (const row of this.grid) {
+      console.log(row.join(""));
+    }
   }
 }
+const tablero = new GameOfLife(4, 8);
+tablero.printGrid();
