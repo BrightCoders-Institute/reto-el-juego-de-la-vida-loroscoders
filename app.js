68bda62efa8a1444ed6bbe63f5e3b51a1fd4d8f0
DerekAyala <derek.ayala12345@hotmail.com>
2023-05-31 18:31:12 -0600

    Apply ESLint recommended changes
    This commit implements the recommended changes provided by ESLint. By following these guidelines, we ensure consistent code quality and adherence to best practices. The changes include fixing code style issues, resolving syntax errors, and improving overall code readability. This update aligns our codebase with industry standards and enhances maintainability.
    Co-authored-by: FebernalGober <alexis.ferber18@gmail.com>
    Co-authored-by: Tole15 <rodrigermoon@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+/**
+  * Clase del juego de la vida
+  * @param {number} this.rows - el número de filas en el tablero.
+  * @param {number} this.columns - el número de columnas en el tablero.
+  * @param {Array<Array<string>>} this.grid - tablero del juego.
+  * @param {number} this.generation - el numero de generacion.
+ */
 class GameOfLife {
+  /**
+   * crea una instancia del juego de la vida.
+   *
+   * @param {function} this.validateColumns - Funcion para validar las columnas.
+   * @param {function} this.validateRows - Funcion para validar las filas.
+   * @param {number} rows - el número de filas en el tablero.
+   * @param {number} columns - el número de columnas en el tablero.
+   */
   constructor(rows, columns) {
     this.validateRows(rows);
     this.validateColumns(columns);
@@ -9,29 +24,66 @@ class GameOfLife {
     this.generation = 1;
   }
 
+
+  /**
+   * Funcion para crear el tablero
+   *
+   * @param {Array<Array<string>>} this.grid - tablero del juego.
+   * @param {number} rows - el número de filas en el tablero.
+   * @param {number} columns - el número de columnas en el tablero.
+   * @return {Array<Array<string>>} - la copia del tablero.
+   */
   createGrid() {
     const grid = new Array(this.rows);
     for (let i = 0; i < this.rows; i++) {
       grid[i] = new Array(this.columns);
       for (let j = 0; j < this.columns; j++) {
-        grid[i][j] = Math.random() < 0.5 ? "." : "*";
+        grid[i][j] = Math.random() < 0.5 ? '.' : '*';
       }
     }
     return grid;
   }
 
+  /**
+   * Funcion para validar las filas.
+   *
+   * @param {number} rows - las filas del tablero.
+   * @throws {Error} - error si no es una entrada valida.
+   */
   validateRows(rows) {
-    if (typeof rows !== 'number' || rows <= 0 || isNaN(rows)) {
-      throw new Error("Las Filas deben de ser un numero valido y mayor que 0");
+    if (typeof rows !== 'number') {
+      throw new Error('Las filas deben ser en número.');
+    }
+    if (rows <= 0) {
+      throw new Error('Las filas deben ser positivos.');
+    }
+    if (isNaN(rows)) {
+      throw new Error('Las filas no deben de estar vacias.');
     }
   }
 
+  /**
+   * Funcion para validar las columnas.
+   *
+   * @param {number} columns - las Columnas del tablero.
+   * @throws {Error} - error si no es una entrada valida.
+   */
   validateColumns(columns) {
-    if (typeof columns !== 'number' || columns <= 0 || isNaN(columns)) {
-      throw new Error("Las Columnas deben de ser un numero valido y mayor que 0");
+    if (typeof columns !== 'number') {
+      throw new Error('Las columnas deben de ser en número');
+    }
+    if (columns <= 0 ) {
+      throw new Error('Las Columnas deben de ser positivos');
+    }
+    if (isNaN(columns)) {
+      throw new Error('Las Columnas no deben de estar vacías');
     }
   }
 
+  /**
+   * Funcion para generar una copia del tablero.
+   * @return {Array<Array<string>>} la copia del tablero.
+   */
   createCopy() {
     const copy = new Array(this.rows);
     for (let i = 0; i < this.rows; i++) {
@@ -43,6 +95,10 @@ class GameOfLife {
     return copy;
   }
 
+  /**
+   * Funcion recorrer el tablero del juego de la vida.
+   * @param {Array<Array<string>>} copia - copia del tablero del juego.
+   */
   nextGeneration() {
     const copia = this.createCopy();
     for (let i = 0; i < this.rows; i++) {
@@ -53,38 +109,46 @@ class GameOfLife {
     this.generation++;
   }
 
-
-  cellsAround(fila, columna, copia) {
+  /**
+   * Verifica las celdas vecinas de una posición dada.
+   * @param {number} rows - La fila de la posición.
+   * @param {number} columns - La columna de la posición.
+   * @param {Array<Array<string>>} copia - La fila de la posición.
+   */
+  cellsAround(rows, columns, copia) {
     let cellsAlive = 0;
-    for (let i = fila - 1; i <= fila + 1; i++) {
-      for (let j = columna - 1; j <= columna + 1; j++) {
-        // Verificar que la posición esté dentro de los límites de la matriz y no sea la posición original
-        if (i >= 0 && i < this.rows && j >= 0 && j < this.columns && (i !== fila || j !== columna)) {
-          if (copia[i][j] === "*") {
+    for (let i = rows - 1; i <= rows + 1; i++) {
+      for (let j = columns - 1; j <= columns + 1; j++) {
+        if (i >= 0 && i < this.rowss && j >= 0 &&
+          j < this.columns && (i !== rows || j !== columns)) {
+          if (copia[i][j] === '*') {
             cellsAlive++;
           }
         }
       }
     }
 
-    if (this.grid[fila][columna] === "*") {
+    if (this.grid[fila][columna] === '*') {
       if (cellsAlive <= 1) {
-        this.grid[fila][columna] = "."
+        this.grid[fila][columna] = '.';
       }
       if (cellsAlive > 3) {
-        this.grid[fila][columna] = "."
+        this.grid[fila][columna] = '.';
       }
     } else {
       if (cellsAlive == 3) {
-        this.grid[fila][columna] = "*"
+        this.grid[fila][columna] = '*';
       }
     }
   }
 
+  /**
+   * Funcion para imprimir el tablero del juego de la vida.
+   */
   printGrid() {
-    console.log("Generation ", this.generation);
+    console.log('Generation ', this.generation);
     for (const row of this.grid) {
-      console.log(row.join(""));
+      console.log(row.join(''));
     }
   }
 }
